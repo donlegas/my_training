23ce739f200e60f8ba8b78e08270263aabf400fb
donlegas <donlegas@gmail.com>
2024-08-31 16:50:54 +0300

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,13 @@
+number_DZ = 12
+print(number_DZ)
+
+number_of_hours_spent = 1.5
+print(number_of_hours_spent)
+
+name_of_course = 'Phyton'
+print(name_of_course)
+
+time_for_one_task = number_of_hours_spent / number_DZ
+print(time_for_one_task)
+
+print('Курс',(name_of_course),', всего задач', number_DZ,', затрачено часов', number_of_hours_spent,', среднее время выполнения', time_for_one_task,'часа')
