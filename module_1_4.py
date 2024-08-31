bbbabb6c44906f0cc9a083a791d5e64f50c58d6a
donlegas <donlegas@gmail.com>
2024-08-31 21:08:00 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input('')                                  ## ввожу фразу "одинокий дуб в поле стоял"
+# print(my_string)
+
+print(my_string.upper())                               ## результат 'ОДИНОКИЙ ДУБ В ПОЛЕ СТОЯЛ'
+
+print(my_string.lower())                               ## результат 'одинокий дуб в поле стоял'
+
+print(my_string.replace(' ', ''))          ## результат 'одинокийдубвполестоял'
+
+print(my_string[0])                                    ## результат 'о'
+
+print(my_string[-1])                                   ## результат 'л'
